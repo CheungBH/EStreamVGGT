8db927c1f603f4a2275ccea944919340c908b8a8
cheungbh <534660436@qq.com>
2026-03-08 08:09:21 +0800

    fix jinitaimei

--- src/vggt/models/aggregator.py
+++ src/vggt/models/aggregator.py
@@ -199,13 +199,16 @@ class Aggregator(nn.Module):
                 and the patch_start_idx indicating where patch tokens begin.
         """
         B, S, C_in, H, W = images.shape
-        # Normalize images and reshape for patch embed
-        images = (images - self._resnet_mean.to(device=images.device, dtype=images.dtype)) / self._resnet_std.to(device=images.device, dtype=images.dtype)
+        target_dtype = (
+            next(self.patch_embed.patch_embed.parameters()).dtype
+            if hasattr(self.patch_embed, "patch_embed")
+            else next(self.patch_embed.parameters()).dtype
+        )
+        images = images.to(target_dtype)
+        images = (images - self._resnet_mean.to(device=images.device, dtype=target_dtype)) / self._resnet_std.to(device=images.device, dtype=target_dtype)
 
         # Reshape to [B*S, C, H, W] for patch embedding
         images = images.reshape(B * S, C_in, H, W)
-        target_dtype = next(self.patch_embed.parameters()).dtype
-        images = images.to(target_dtype)
         patch_tokens = self.patch_embed(images)
 
         if isinstance(patch_tokens, dict):
